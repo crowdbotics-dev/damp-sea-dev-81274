572fd708e0ac1d93d9b3875ae47864f0760047be
Crowdbotics <engineering@crowdbotics.com>
2023-05-05 00:43:57 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/damp_sea_dev_81274/MainActivity.java
@@ -0,0 +1,15 @@
+package com.damp_sea_dev_81274;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "damp_sea_dev_81274";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "damp-sea-dev-81274",
+  url: "https://damp-sea-dev-81274.botics.co",
+  api: "https://damp-sea-dev-81274.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
